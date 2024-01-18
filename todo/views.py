e1fe9df11e3886064758f1dc605ffac0ffa2eac7
John Schmidt <131378485+oktopusboss@users.noreply.github.com>
2024-01-18 16:50:24 -0600

    Add files via upload

---
+++ todo/views.py
@@ -0,0 +1,106 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import View
+from django.urls import reverse
+from .forms import TaskForm
+from .models import TaskModel
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+import json
+
+# Create your views here.
+
+# tasks = [
+#     {"id": 1, "name": "Make video", "description": "make vid for course", "priority": 1}, 
+#     {"id": 2, "name": "Do dishes", "description": "wash dishes", "priority": 2},
+#     {"id": 3, "name": "Hit the griddy", "description": "hit the griddy", "priority": 3}]
+
+# def get_task_by_id(task_id):
+#     for task in tasks:
+#         if task["id"] == task_id:
+#             return task
+#     return None
+
+@login_required(login_url='todo:login')
+def index(request):
+    task_form = TaskForm()
+    # print(request.session['tasks'])
+    # if 'tasks' not in request.session:
+    #     request.session['tasks'] = []
+    tasks = TaskModel.objects.filter(user=request.user)
+    # print(request.user)
+    # ctx = {"tasks": request.session['tasks'], "form": taskform}
+    ctx = {"tasks": tasks, "form": task_form, "user": request.user}
+    return render(request, "todo/index.html", ctx)
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect(reverse('todo:index'))
+
+    return render(request, 'todo/register.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('todo:index'))
+        else:
+            return render(request, 'todo/login.html')
+
+    return render(request, 'todo/login.html')
+
+
+@login_required()
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('todo:index'))
+
+def delete_task(request):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        task_id = body['taskId']
+        task = TaskModel.objects.get(id=task_id)
+        task.delete()
+        return HttpResponseRedirect(reverse('todo:index'))
+    return HttpResponseRedirect(reverse('todo:index'))
+
+def add_task(request):
+    # print(request.POST)
+    # request.session["hello"] = "world"
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            task = TaskModel(
+                user=request.user,
+                name=form.cleaned_data['task'],
+                description=form.cleaned_data['description'],
+                priority=int(form.cleaned_data['priority']),)
+            # request.session['tasks'].append(task)
+            task.save()
+            # request.session.save()
+    return HttpResponseRedirect(reverse('todo:index'))
+
+def task(request, id):
+    task = TaskModel.objects.get(id=id)
+    ctx = {"task": task}
+    return render(request, "todo/task.html", ctx)
+
+class AboutView(View):
+    def get(self, request):
+        return HttpResponse("This is about page")
+    
+
